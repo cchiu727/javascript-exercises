02bff534aa69071b000b014149c45d4c0d1cc25e
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-09-20 22:16:01 -0700

    Add solution for 07 (passes all tests)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let cels = (fahr - 32) * (5 / 9);
+  return Math.round(cels * 10) / 10; // round to 1 decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let fahr = (cels * (9 / 5)) + 32;
+  return Math.round(fahr * 10) / 10; // round to 1 decimal place
 };
 
 // Do not edit below this line
