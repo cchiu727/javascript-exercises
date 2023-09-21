c0dae0e94541c7aaa15fa3707eeb3f9303f20244
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-09-20 20:19:02 -0700

    Add solution for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
 
+    for (let i = 1; i < arguments.length; i++) {
+        arr = arr.filter((number) => {
+            return number !== arguments[i];
+        });
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
