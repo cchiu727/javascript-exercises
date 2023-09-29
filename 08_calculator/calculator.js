ad97755707fead91bd3a6dfa48d92bedf7cc1493
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-09-29 14:27:25 -0700

    Add solutions for 08_calculator (all tests passed)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = 1;
 
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+
+  return product
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+	return x ** n;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  
+  for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
